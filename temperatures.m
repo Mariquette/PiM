c7b0d402f5355180a1c3fe6297bb07ba9b128467
Martin Urban <urbanm24@Ufel.cvut.cz>
2017-03-27 13:25:21 +0200

    Uprava osy x na format HH: MM Prolozeni interp1

--- temperatures.m
+++ temperatures.m
@@ -2,10 +2,20 @@ clear all; close all; clc;
 
 temp = load('temperatures.txt');
 
+h =num2str(temp(:,1)); m = num2str(temp(:,2));
+a = strcat(h,':',m);
+time=datenum(a, 'HH:MM');
+
+inttime = linspace(time(1),time(19),120);
+inttemp = interp1(time,temp(:,3),inttime,'pchip');
 %%
 figure(1);
+hold on
+plot(time,temp(:,3),'x');
+plot(inttime,inttemp);
+datetick('x','HH:MM')
 grid on;
 grid minor;
-plot(temp(:,1)+temp(:,2)/60,temp(:,3),'o-');
-xlabel('time');
-ylabel('temperature (°C)');
+xlabel('Time (HH:MM)');
+ylabel('Temperature (°C)');
+ylim([17 35])
